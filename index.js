aee75f78cc0d5f99d47a868f5ac3d3b60409992f
zatziky <zatziky@gmail.com>
2018-07-09 17:25:57 +0200

    feat: expose chatbot-cache

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 module.exports.Chatbot = require('./lib/chatbot/chatbot')
 module.exports.State = require('./lib/chatbot/state')
+module.exports.chatbotCache = require('./lib/chatbot/chatbot-cache')
