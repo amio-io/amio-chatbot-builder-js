c83bc242ad3e055f2dc8c0f7fa6d32341898384e
zatziky <zatziky@gmail.com>
2018-07-11 11:02:45 +0200

    ref: get*(webhook) moved to a separate 2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # amio-chatbot-builder-js
+
+## Installation
+
+```bash
+npm install amio-chatbot-builder --save
+```
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.Chatbot = require('./lib/chatbot/chatbot')
 module.exports.State = require('./lib/chatbot/state')
+module.exports.Interceptor = require('./lib/chatbot/interceptor')
 module.exports.chatbotCache = require('./lib/chatbot/chatbot-cache')
